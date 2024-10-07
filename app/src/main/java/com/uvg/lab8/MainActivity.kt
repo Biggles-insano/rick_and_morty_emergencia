a5b5398399f5b6be7cd645fe674be071bdc8534d
Samuel Mejía <150869146+Biggles-insano@users.noreply.github.com>
2024-10-06 18:39:24 -0600

    Update MainActivity.kt

--- app/src/main/java/com/uvg/lab8/MainActivity.kt
+++ app/src/main/java/com/uvg/lab8/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.uvg.lab8.screens.*
 import androidx.navigation.NavType
@@ -28,14 +30,24 @@ class MainActivity : ComponentActivity() {
 fun RickAndMortyApp() {
     val navController = rememberNavController()
 
+  
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = backStackEntry?.destination?.route
+
     Scaffold(
-        bottomBar = { RickAndMortyBottomNavigation(navController) } // Agrega el BottomNavigation aquí
+        bottomBar = {
+            
+            if (currentRoute != "login") {
+                RickAndMortyBottomNavigation(navController)
+            }
+        }
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = "character_list", // Cambia esto si deseas otra pantalla inicial
+            startDestination = "login",  
             modifier = Modifier.padding(innerPadding)
         ) {
+            composable("login") { LoginScreen(navController) } 
             composable("character_list") { CharacterListScreen(navController) }
             composable("locations") { LocationsScreen(navController) }
             composable("profile") { ProfileScreen(navController) }
@@ -61,19 +73,19 @@ fun RickAndMortyApp() {
 fun RickAndMortyBottomNavigation(navController: NavHostController) {
     BottomNavigation {
         BottomNavigationItem(
-            icon = {}, // Pasamos un ícono vacío
+            icon = {}, 
             label = { Text("Characters") },
             selected = false,
             onClick = { navController.navigate("character_list") }
         )
         BottomNavigationItem(
-            icon = {}, // Pasamos un ícono vacío
+            icon = {}, 
             label = { Text("Locations") },
             selected = false,
             onClick = { navController.navigate("locations") }
         )
         BottomNavigationItem(
-            icon = {}, // Pasamos un ícono vacío
+            icon = {}, 
             label = { Text("Profile") },
             selected = false,
             onClick = { navController.navigate("profile") }
